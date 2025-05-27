856707a00b4074f187fdc82252c5ca759571bb50
BAO10022004 <Giabaoonthcs123@gmail>
2025-05-27 18:08:24 +0700

    change databasedatabase

--- src/main/java/com/example/QL_KHOAHOC/controller/AccountController.java
+++ src/main/java/com/example/QL_KHOAHOC/controller/AccountController.java
@@ -1,15 +1,22 @@
 package com.example.QL_KHOAHOC.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.QL_KHOAHOC.Service.AccountService;
 import com.example.QL_KHOAHOC.dtoRequest.AccountDto;
 import com.example.QL_KHOAHOC.dtoRequest.SignInRequest;
 import com.example.QL_KHOAHOC.entity.Account;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts") // Thêm tiền tố /api/accounts cho tất cả các endpoint trong controller này
@@ -70,7 +77,7 @@ public class AccountController {
         if (signedInAccount != null) {
             return new ResponseEntity<>(signedInAccount, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // trả về 401
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 }
